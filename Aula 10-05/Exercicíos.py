81aea0b08b9f4c991e41122d9732714fd33612aa
Matheus Rodrigues de OLiveira <matheusrodriguesoliveira273@gmail.com>
2021-05-11 14:45:54 -0300

    :tada: Resolvendo mais exercicíos

--- Aula 10-05/Exercicíos.py
+++ Aula 10-05/Exercicíos.py
@@ -37,3 +37,77 @@ n2 = float(input("Digite o imposto(apenas o número): "))
 
 resultado = somaImposto(n2, n1)
 print("O preço com impostos é: ", resultado)
+
+# Exercicío 4
+
+
+def salario(horasTrabalhadas, valor_hora):
+    salario = horasTrabalhadas * valor_hora
+    print("Você irá receber: R$", salario)
+    if horasTrabalhadas > 40:
+        hextra = horasTrabalhadas - 40
+        hextra = hextra * (valor_hora * 1.5)
+        salario = salario + hextra
+        print("\nO valor da hora extra é: ", hextra)
+        print("Você irá receber: R$", salario)
+
+
+valor_hora = float(input("Quanto você recebe por hora? \n"))
+horasTrabalhadas = float(input("\nQuantas horas você trabalho? \n"))
+salario(horasTrabalhadas, valor_hora)
+
+# Exercicío 5
+
+
+def IMC(altura, peso):
+    imc = peso / altura**2
+    print("Seu imc é: %.2f" % imc)
+
+
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite o seu peso: "))
+
+IMC(altura, peso)
+
+# Exercucío 6
+
+
+def conversor(nota):
+    if nota >= 9.0:
+        print("A")
+    elif nota >= 8.0:
+        print("B")
+    elif nota >= 7.0:
+        print("C")
+    elif nota >= 6.0:
+        print("D")
+    elif nota >= 4.1:
+        print("E")
+    elif nota <= 4.0:
+        print("F")
+
+
+nota = float(input("Digite sua nota: "))
+
+conversor(nota)
+
+# Exercicío 7
+
+
+def numeros(numero1, numero2):
+    min = numero1
+
+    if numero2 < min:
+        min = numero2
+        print("O menor número é: ", min)
+    elif numero1 == numero2:
+        print("Números iguais")
+    else:
+        min = numero1
+        print("O menor número é: ", min)
+
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+numeros(n1, n2)
