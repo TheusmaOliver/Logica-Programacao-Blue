d2025a2481273cd47d1c0181da8a3ee4dde52dc2
Matheus Rodrigues de OLiveira <matheusrodriguesoliveira273@gmail.com>
2021-05-24 13:38:42 -0300

    Corrigindo exercicio

--- Aula_14/exercicio4.py
+++ Aula_14/exercicio4.py
@@ -1,4 +1,10 @@
-def transform(date):
+from datetime import date
+
+
+def verifica(date):
+    global day
+    global year
+    global atual
     global indexMonth
 
     meses = ['', 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
@@ -6,12 +12,6 @@ def transform(date):
              'outubro', 'novembro', 'dezembro']
     month = meses[indexMonth]
 
-    return month
-
-
-def verifica(date):
-    global day
-    global year
     if (len(date) < 10 and len(date) > 10):
         print('Null')
         return date
@@ -21,15 +21,31 @@ def verifica(date):
     elif (indexMonth < 1 or indexMonth > 12):
         print('Null')
         return date
+    elif day < 1 or day > 31:
+        print('Null')
+    elif year > atual:
+        print('Null')
+    elif month == 'fevereiro' and day > 28 and year % 4 == 1:
+        print("Fevereiro so tem 28 dias em anos não bissextos!")
+    elif month == 'fevereiro' and day >= 30:
+        print('Fevereiro so tem 29 ou 28 dias!')
+    elif month == 'abril' and day > 30:
+        print('Abril so tem 30 dias!')
+    elif month == 'junho' and day > 30:
+        print('Junho so tem 30 dias!')
+    elif month == 'setembro' and day > 30:
+        print('Setembro so tem 30 dias!')
+    elif month == 'novembro' and day > 30:
+        print('Novembro so tem 30 dias!')
     else:
-        print(f'{day} de {transform(date)} de {year}')
+        print(f'{day} de {month} de {year}')
 
 
-date = input("Digite uma data (DD/MM/AAAA): ")
+data = input("Digite uma data (DD/MM/AAAA): ")
 
-day = int(date[0:2])
-indexMonth = int(date[3:5])
-year = int(date[6:10])
+day = int(data[0:2])
+indexMonth = int(data[3:5])
+year = int(data[6:10])
+atual = date.today().year
 
-
-verifica(date)
+verifica(data)
